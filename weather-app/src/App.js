a4ee7a20e47c16b985e2a448179e526d9070fca5
Markus <markuspokkinen@gmail.com>
2019-04-17 07:27:06 +0300

    current weather is shown in text

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Forecast from './ForcastWeather.js';
 import Current from './CurrentWeather.js';
 
@@ -7,7 +6,12 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            curWeath: {
+                name: "",
+                weather: [{}],
+                main: {}
+            },
+            forcWeath: { list: [{}]}
         }
     }
 
@@ -20,30 +24,23 @@ export default class App extends Component {
     }
 
     getdata = (position) => {
-        console.log(position)
-        let curWeath;
-        let forcWeath;
 
         fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=9592eb101cb5b0e09de21ab8f991d0c3&units=metric")
             .then(res => res.json())
             .then(res => {
-                console.log(res)
-                curWeath = res;
-            }).then(res => fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&APPID=9592eb101cb5b0e09de21ab8f991d0c3&units=metric")
-            .then(res => res.json())
-            .then(res => {
-                console.log(res)
-                forcWeath = res;
-
-                })).then(res => {
-
+                this.setState({
+                    curWeath: res,
+                    forcWeath: this.state.forcWeath
+                })
+            })
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=9592eb101cb5b0e09de21ab8f991d0c3&units=metric")
+                .then(res => res.json())
+                .then(res => {
                     this.setState({
-                        curWeath: curWeath,
-                        forcWeath: forcWeath
-                    }, () => {
-                        console.log(this.state)
-                    });
-            });
+                        curWeath: this.state.curWeath,
+                        forcWeath: res
+                    }, () => { console.log(this.state) })
+                });
         
     }
 
--- weather-app/src/CurrentWeather.js
+++ weather-app/src/CurrentWeather.js
@@ -2,11 +2,32 @@
 
 export default class CurrentWeather extends Component {
 
+    constructor(props) {
+        super(props)
+        console.log(props.data)
+        this.state = {
+            weath:this.props.data
+        }
+    }
+    componentWillReceiveProps(nextprops) {
+        if (this.props !== nextprops) {
+            this.setState({
+                weath: nextprops.data
+            }, () => { console.log(this.state) })
+        }
+    }
+
 
     render() {
         return (
             <div>
-                <p>Current</p>
+                <p>Current:</p>
+                <p> {this.state.weath.name}, {this.state.weath.weather[0].main}</p>
+                <p>temp:{this.state.weath.main.temp} C</p>
+                <p>humidity: {this.state.weath.main.humidity}</p>
+                <p>pressure: {this.state.weath.main.pressure}</p>
+                <p>temp max: {this.state.weath.main.temp_max} C</p>
+                <p>temp min: {this.state.weath.main.temp_min} C</p>
             </div>
         )
     }
