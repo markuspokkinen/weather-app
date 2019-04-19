2a411a637e2003641093b42273889db9f7a73362
Markus Pokkinen <markuspokkinen@gmail.com>
2019-04-19 10:03:59 +0300

    forcast päivät jaettu omiin arrayhin

--- weather-app/src/ForcastWeather.js
+++ weather-app/src/ForcastWeather.js
@@ -1,58 +1,102 @@
 ﻿import React, { Component } from 'react';
 import WeathIm from './weatherImage';
+import OneDay from './oneday';
 import './forecastWeather.css';
 
 export default class forcastWeather extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [{ dt: "", main: {}, weather: [{}], dt_txt: "" }]
+            list: [{ dt: "", main: {}, weather: [{}], dt_txt: "" }],
+            days: [{}]
         }
+        this.getWeatherdata();
+    }
+    componentWillReceiveProps(nextprops) {
+        this.getWeatherdata();
+
+    }
+    getWeatherdata() {
         fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + this.props.lat + "&lon=" + this.props.lon + "&APPID=9592eb101cb5b0e09de21ab8f991d0c3&units=metric")
             .then(response => response.json())
             .then(res => {
+                var list = res.list;
+                //console.log(list);
+                var alldays = list.map((oneElem) => oneElem.dt_txt.split(" ")[0]);
+                var days = alldays.filter((v, i) => alldays.indexOf(v) === i);
+                var daysArray = [];
+                //console.log(days);
+
+                for (var i = 0; i < days.length; i++) {
+                    var element = [];
+                    for (var x = 0; x < list.length; x++) {
+                        if (list[x].dt_txt.includes(days[i])) {
+                            element.push(list[x]);
+                        }
+                    }
+                    daysArray.push(element);
+                }
+
+                console.log(daysArray);
+
                 this.setState({
-                    list: res.list
-                });
-            });
-    }
-    componentWillReceiveProps(nextprops) {
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + nextprops.lat + "&lon=" + nextprops.lon + "&APPID=9592eb101cb5b0e09de21ab8f991d0c3&units=metric")
-            .then(response => response.json())
-            .then(res => {
-                this.setState({
-                    list: res.list
+                    list: res.list,
+                    days: daysArray
                 })
             });
 
     }
-    dateortime = (ent, ind) => {
+    dateortimefunc = (ent, ind) => {
         if (ind % 2 === 0) {
-            return <td>{ent}</td>
+            return <td key={"date: [" + ind + "]"}>{ent}</td>
         } else {
-            return <td>{ent}</td>
+            return <td key={"time: [" + ind + "]"}>{ent}</td>
         }
     }
 
     render() {
         return (
             <div id="forcast-main">
-                
-                <table>
-                    <tr>
-                        <th colSpan="7"><h2>Forecast</h2></th>
-                    </tr>
-                    {this.state.list.map((ent) =>
-                        <tr>
-                            {ent.dt_txt.split(" ").map((entry, index) => this.dateortime(entry, index))}
-                            <td>{ent.weather[0].main}</td>
-                            <td><WeathIm icon={ent.weather[0].icon} /></td>
-                            <td> {Math.round(ent.main.temp)} &#8451;</td>
-                            <td> max: {Math.round(ent.main.temp_max)} &#8451;</td>
-                            <td>min: {Math.round(ent.main.temp_min)} &#8451;</td>
 
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan="100000"><h2>Today</h2></th>
                         </tr>
-                    )}
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {this.state.list.map((entry) => {
+                                return <td key={entry.dt_txt}>
+                                    {entry.dt_txt}
+
+
+
+                                </td>
+                            })
+
+
+                            }
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th colSpan="100000"><h2>Forecast</h2></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.list.map((ent) =>
+                            <tr key={"table:" + ent.dt_txt}>
+                                {ent.dt_txt.split(" ").map((entry, index) => this.dateortimefunc(entry, index))}
+                                <td>{ent.weather[0].main}</td>
+                                <td><WeathIm icon={ent.weather[0].icon} /></td>
+                                <td> {Math.round(ent.main.temp)} &#8451;</td>
+                                <td> max: {Math.round(ent.main.temp_max)} &#8451;</td>
+                                <td>min: {Math.round(ent.main.temp_min)} &#8451;</td>
+
+                            </tr>
+                        )}
+                    </tbody>
                 </table>
             </div>
         )
---
+++ weather-app/src/oneday.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+
+export default class oneday extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            day: "",
+            list: [{}]
+        };
+    }
+
+
+    render() {
+        return (
+            <tr>
+
+            </tr>
+        );
+    }
+
+}
